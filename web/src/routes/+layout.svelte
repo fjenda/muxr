40a3b32d90d34e0f7d17fa9c8fdc60372d93af57
dhonus <dhonus@tuta.io>
2025-07-17 17:14:05 +0200

    feat: basic navbar

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,11 +1,29 @@
 <script>
     import { LoadingProvider, DialogProvider } from "$providers";
+    import { faWaveSquare } from "@fortawesome/free-solid-svg-icons";
+    import { faGithub } from "@fortawesome/free-brands-svg-icons";
+    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
     let { children } = $props();
 </script>
 
 <LoadingProvider>
     <DialogProvider>
         <main class="container">
+            <nav>
+                <ul>
+                    <li><a href="/">
+                        <FontAwesomeIcon icon={faWaveSquare} size="1x"/>
+                        muxr
+                    </a></li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="https://github.com/fjenda/muxr" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithub} size="1x"/>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
             {@render children()}
         </main>
     </DialogProvider>
@@ -17,6 +35,26 @@
         height: 100%;
         display: flex;
         flex-direction: column;
-        padding: 1rem;
+    }
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background: #0f0f0f;
+        padding: .5rem 0;
+        font-size: 1.2rem;
+    }
+    nav ul {
+        list-style: none;
+        display: flex;
+        gap: 1rem;
+        color: white;
+        margin: 0;
+        padding: 0 2rem;
+    }
+    nav a {
+        text-decoration: none;
+        color: white;
+        font-weight: bold;
     }
 </style>
